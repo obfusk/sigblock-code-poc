af5d2e0da9d8a2f0882e94a4982f13e2063f65ef
Felix C. Stegerman <flx@obfusk.net>
2023-01-27 01:33:53 +0100

    ...

---
+++ app/src/main/java/dev/obfusk/sbcodepoc/Encoding.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2023 FC Stegerman <flx@obfusk.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.obfusk.sbcodepoc
+
+import java.io.RandomAccessFile
+
+internal fun RandomAccessFile.read(length: Int): ByteArray {
+    val buffer = ByteArray(length)
+    var offset = 0
+    while (offset < length) {
+        val result = read(buffer, offset, length - offset)
+        if (result < 0) {
+            error("Not enough bytes to read: $length")
+        }
+        offset += result
+    }
+    return buffer
+}
+
+internal fun RandomAccessFile.readUInt(numberOfBytes: Int): Long {
+    val buffer = read(numberOfBytes)
+    var value: Long = 0
+    for (k in 0 until numberOfBytes) {
+        val next = buffer[k].toUByte().toLong()
+        value += next shl k * java.lang.Byte.SIZE
+    }
+    return value
+}
--- app/src/main/java/dev/obfusk/sbcodepoc/MainActivity.kt
+++ app/src/main/java/dev/obfusk/sbcodepoc/MainActivity.kt
@@ -1,11 +1,62 @@
+// SPDX-FileCopyrightText: 2023 FC Stegerman <flx@obfusk.net>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 package dev.obfusk.sbcodepoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.io.RandomAccessFile
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apk = getAPK()
+        Log.v(TAG, "APK: ${apk}")
+        // ...
     }
+
+    fun getAPK(): String {
+        val pm = applicationContext.getPackageManager()
+        val info = pm.getApplicationInfo("dev.obfusk.sbcodepoc", 0)
+        return info.sourceDir
+    }
+
+    fun getSB(apk: String): ByteArray? {
+        RandomAccessFile(apk, "r").use {
+            val len = it.length()
+            var pos = len - 1024
+            while (pos < len - 1) {
+                it.seek(pos)
+                val eocd_magic = it.read(4)
+                if (eocd_magic.contentEquals(EOCD_MAGIC)) {
+                    it.seek(pos - 16)
+                    val cd_off = it.readUInt(4)
+                    it.seek(cd_off - 16)
+                    val sb_magic = it.read(4)
+                    if (!sb_magic.contentEquals(SB_MAGIC)) {
+                        error("No APK Signing Block")
+                    }
+                    it.seek(cd_off - 8)
+                    val sb_size2 = it.readUInt(8)
+                    it.seek(cd_off - sb_size2)
+                    val sb_size1 = it.readUInt(8)
+                    if (sb_size1 != sb_size2) {
+                        error("APK Signing Block sizes not equal")
+                    }
+                    it.seek(cd_off - sb_size2)
+                    val sig_block = it.read(sb_size2.toInt() + 8)
+                    return sig_block
+                }
+                ++pos
+            }
+        }
+        return null
+    }
+
+    val EOCD_MAGIC = byteArrayOf(0x50, 0x4b, 0x05, 0x06)
+    val SB_MAGIC = "APK Sig Block 42".toByteArray()
+    val TAG = "SBCodePoC"
 }
